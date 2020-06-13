7b6c3200e489e4bfc7f454a4fab2d9b70b8d42f7
Kiki1985 <miroslav.dojchinovic@gmail.com>
2020-06-13 16:26:46 +0200

    send mail when submit comment and response to authors and users email adress - model events and listeners

--- app/Comment.php
+++ app/Comment.php
@@ -2,10 +2,6 @@
 
 namespace App;
 
-//use App\Mail\CommentCreated;
-
-//use Illuminate\Support\Facades\Mail;
-
 class Comment extends Model
 {
     public function article()
@@ -22,14 +18,4 @@ class Comment extends Model
     {
         return $this->hasMany(Response::class);
     }
-
-    /*protected static function boot()
-    {
-        parent::boot();
-        static::created(function ($comment){
-            Mail::to($comment->article->user->email)->send(
-                new CommentCreated($comment)
-            );
-        });
-    }*/
 }
--- app/Response.php
+++ app/Response.php
@@ -2,10 +2,6 @@
 
 namespace App;
 
-//use App\Mail\ResponseCreated;
-
-//use Illuminate\Support\Facades\Mail;
-
 class Response extends Model
 {
     public function comment()
@@ -17,18 +13,4 @@ class Response extends Model
     {
         return $this->belongsTo(User::class, 'author_id');
     }
-
-    /*protected static function boot()
-    {
-        parent::boot();
-        static::created(function ($response){
-            \Mail::to($response->comment->user->email)->send(
-                new ResponseCreated($response)
-            );
-
-            \Mail::to($response->comment->article->user->email)->send(
-                new ResponseCreated($response)
-            );
-        });
-    }*/
 }
